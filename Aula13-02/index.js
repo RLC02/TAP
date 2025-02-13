a4bb1b9ea79d8acdf419c8a0818c78ad48de7489
Ricardo Luquetti <99838772+RLC02@users.noreply.github.com>
2025-02-13 20:47:19 -0300

    Aula13/02

---
+++ Aula13-02/index.js
@@ -0,0 +1,3 @@
+var soma = require("./exportacao_de_modulos.js");
+
+console.log("Soma:", soma(5, 10));
---
+++ Aula13-02/operadores_logicos.js
@@ -0,0 +1,12 @@
+var n1 = 10;
+var n2 = 10;
+
+var total = n1 + n2;
+var totalmulti = n1 * n2;
+var totaldiv = n1 / n2;
+var totalmod = n1 % n2;
+
+console.log("Total:", total);
+console.log("Multiplication:", totalmulti);
+console.log("Division:", totaldiv);
+console.log("Modulo:", totalmod);
